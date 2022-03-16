aa35fbf185ec5355b02a9affca1b9e67d376f0b5
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2022-03-16 12:32:43 +0300

    update app config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
       title: `new`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
+      },
+    }
+  ],
 };
