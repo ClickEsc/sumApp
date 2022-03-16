cddddeeb21474d1ef6305e32990cdc355acb8805
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2022-03-16 23:56:28 +0300

    Updates

---
+++ component---src-pages-addition-js-1838ad5e14c0d1cad557.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksum_app=self.webpackChunksum_app||[]).push([[300],{7318:function(e,t,n){n.r(t);var c=n(7294),p=n(9499),s=(n(4349),n(7598)),u=n(7029),a=n(3174);t.default=function(){return console.log("process.env.GATSBY_PUBLIC_URL","https://clickesc.github.io/sumApp"),c.createElement(s.Z,null,c.createElement(p.Router,{basepath:"/"},c.createElement(u.Z,{path:"/addition",component:a.Z})))}}}]);
+//# sourceMappingURL=component---src-pages-addition-js-1838ad5e14c0d1cad557.js.map
