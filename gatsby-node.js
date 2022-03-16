03a5a625c180ba135e964b213e4a9ac38e7e8ea7
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2022-03-16 21:36:31 +0300

    fix webpack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,9 @@
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === "build-html" || stage === "develop-html") {
     actions.setWebpackConfig({
+      resolve: {
+        extensions: ['.js', '.jsx']
+      },
       module: {
         rules: [
           {
